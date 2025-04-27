cb37c9e6895be9025d48d572046c60c98c24f389
Aryan Singla <singlaaryan2001@gmail.com>
2025-04-27 18:27:54 +0530

    small changes

--- index.js
+++ index.js
@@ -5,30 +5,25 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
-import connectDB from './config/db.js';   // You imported DB connection
+import connectDB from './config/db.js';
 
-dotenv.config(); // you must configure dotenv
+dotenv.config();
 
 const app = express();
 
-// Connect to DB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Just for checking if server is working
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
 
 
-// Start Server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
